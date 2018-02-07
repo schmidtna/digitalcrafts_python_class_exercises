f17385cd38daf4dea37e2d7cb75850cd9451ee6a
Nick Schmidt <nickschmidt@Nicks-MacBook-Pro.local>
2018-02-06 20:49:59 -0600

    updated project code

--- python_part1_exercises/py1_day1_exercises_guess_number.py
+++ python_part1_exercises/py1_day1_exercises_guess_number.py
@@ -1,13 +1,35 @@
-secret_number = 5
-print("I am thinking of a number between 1 and 10")
-player_guess = int(input("Try and guess the number! "))
+import random
+secret_number = random.randint(1, 10)
+chances = 5
+
 while True:
-    if player_guess < secret_number:
-        print("Too low, try again!")
-        player_guess = int(input("Guess what the number is "))
-    elif player_guess > secret_number:
-        print("Too high, try again!")
-        player_guess = int(input("Guess what the number is "))
-    else:
-        print("Congrats! You got it!")
-        break
+    print("I am thinking of a number between 1 and 10")
+    print("You have " + str(chances) + " guesses left.")
+    player_guess = int(input("Try and guess the number! "))
+    if player_guess > secret_number:
+        print("Your guess was too high, try again!")
+        chances -= 1
+    elif player_guess < secret_number:
+        print("Your guess was too low, try again!")
+        chances -= 1
+    elif player_guess == secret_number:
+        print("Congrats! You guessed the number!")
+        replay = input("Would you like to play again? ").lower
+        if replay == "yes" or "y":
+            secret_number = random.randint(1, 10)
+            chances = 5
+        else:
+            chances = -1 
+            print("Thanks for playing!")
+            break
+    if chances < 1:
+        print("Sorry, you ran out of chances.")
+        replay = input("Would you like to play again? ").lower
+        if replay == "yes" or "y":
+            secret_number = random.randint(1, 10)
+            chances = 5
+        else:
+            chances = -1
+            print("Thanks for playing!")
+            break
+
