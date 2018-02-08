307b69b9db10ad48e65c55eac5aa652c2618c835
Nick Schmidt <nickschmidt@Nicks-MacBook-Pro.local>
2018-02-07 19:59:58 -0600

    added multi table ex

---
+++ python_part2_exercises/loop_exercises_multi_table.py
@@ -0,0 +1,24 @@
+
+n1 = 1
+for i in range(1, 11):
+    print(n1, " X ", i," = ", (n1 * i))
+
+n2 = 2
+for i in range(1, 11):
+    print(n2, " X ", i," = ", (n2 * i))
+
+n3 = 3
+for i in range(1, 11):
+    print(n3, " X ", i," = ", (n3 * i))
+
+n4 = 4
+for i in range(1, 11):
+    print(n4, " X ", i," = ", (n4 * i))
+
+n5 = 5
+for i in range(1, 11):
+    print(n5, " X ", i," = ", (n5 * i))
+
+n6 = 6
+for i in range(1, 11):
+    print(n6, " X ", i," = ", (n6 * i))
